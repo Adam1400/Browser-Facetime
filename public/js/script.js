11b02dbc50c2c0d65a326bb9191b8f2c230bceb9
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-08-07 02:13:32 -0600

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,64 @@
+const socket = io.connect('/');
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+//myVideo.muted = true;
+
+
+//set up peer
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '443'
+});
+
+//set up audio video stream
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: {channelCount: 2} //2 for sterio 1 for mono
+}).then(stream =>{
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    //answer call
+    peer.on('call', call =>{
+        call.answer(stream);
+        console.log('answer');
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+
+    //emit room with whos in it
+    socket.on('user-connected', userId =>{
+        connectToNewUser(userId, stream);
+    })
+})
+
+//connect peer
+peer.on('open', id =>{
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+//call room with video
+const connectToNewUser = (userId, stream) =>{
+    //console.log('new user joined : '+ userId);
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream =>{
+        addVideoStream(video, userVideoStream);
+    })
+}
+
+
+//play stream
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () =>{
+        video.play()
+    })
+    //attach video to grid element 
+    videoGrid.append(video);
+}
+
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require ('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidv4 } = require('uuid'); //set up id generator
+
+//set up pug engine
+app.set('view engine', 'pug');
+
+//use public files
+app.use(express.static('public'));
+
+//set up peer to peer
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+
+
+
+//generate a unique room
+app.get('/', (req, res) => {
+    res.redirect(`${uuidv4()}`)
+})
+
+
+
+//render room
+app.get('/:room', (req, res) => {
+    var longId = req.params.room;
+    var Id = 'Room: ' + longId.substring(0, 8);
+    res.render('room', {title: Id , roomId: req.params.room });
+})
+
+//establish peer
+app.use('/peerjs', peerServer);
+
+
+
+//open socket listen for other users
+io.on('connection', socket =>{
+    socket.on('join-room' , (roomId, userId) =>{
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+    })
+})
+
+server.listen(8080);
