f307a80a150863c4be2e422442ab5c4225a6e153
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-08-11 15:46:00 -0600

    p

--- server.js
+++ server.js
@@ -43,9 +43,11 @@ io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit("user-connected", userId);
+    
 
     socket.on("disconnect", () => {
       socket.to(roomId).broadcast.emit("user-disconnected", userId);
+      
     });
   });
 });
