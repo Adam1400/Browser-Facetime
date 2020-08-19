e3e99e7e394a85f38ebec822eb5b4b80086d5097
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-08-19 16:56:00 -0600

    Revert "Update script.js"
    This reverts commit afee046f277f696aafbf4ca16de909f1e5d5f2cd.

--- public/js/script.js
+++ public/js/script.js
@@ -51,21 +51,21 @@ socket.on("user-disconnected", (userId) => {
   document.querySelector('.currentOcupancy').innerHTML = "Members: "+(countMembers()-1);
   if (peers[userId]) peers[userId].close();
 
+  setTimeout(function(){ alert("Hello"); }, 3000);
   if(("Members: "+countMembers()) !== (document.querySelector('.currentOcupancy').innerHTML))
     {
-      //experimental migrate host to prevent ghost video
+      //experimental
       location.reload();
     }
-
+  
+  
 });
 
 //brodcast signal
 myPeer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
-  
-  document.querySelector('.currentOcupancy').innerHTML = "Members: "+ countMembers();
   console.log("My ID: "+ id + " | Room: "+ ROOM_ID  + " | Ocupency: "+ countMembers());
-  //cursed
+  document.querySelector('.currentOcupancy').innerHTML = "Members: "+ countMembers();
 });
 
 //join call in progress
@@ -205,7 +205,6 @@ function expand(){
 function countMembers(){
   var members = document.getElementsByTagName("video").length;
   //console.log("Ocupency: "+ members);
-  if(members === 0){ members = 1}
   return members;
 }
 
